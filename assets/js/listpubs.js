706383f273aafc39f022f77527dc3af28317f89c
Evan Peck <emp017@bucknell.edu>
2022-05-20 15:16:22 -0400

    Playing with links and animations

--- assets/js/listpubs.js
+++ assets/js/listpubs.js
@@ -15,6 +15,10 @@ async function main() {
     let pub_data = await getJSON();
     let pubs = pub_data.publications;
 
+    pubs.sort(function(a, b) {
+        return b.year - a.year;
+    });
+
     pubs.forEach(pub => addPub(pub));
 }
 
@@ -35,24 +39,34 @@ function addPub(pub_data) {
     let pub = makeDiv('pub')
     pubs_section.appendChild(pub);
     
+    // Thumbnail link
+    let thumbnail_link = document.createElement('a')
+    thumbnail_link.classList.add('thumbnail')
+    thumbnail_link.href = 'http://www.eg.bucknell.edu/~emp017/'
 
+    // Thumbnail image
     let thumbnail = document.createElement('img')
     thumbnail.classList.add('thumbnail')
     thumbnail.src = THUMBNAIL_PATH + pub_data.thumbnail
+    // Add image to link
+    thumbnail_link.appendChild(thumbnail)
 
+    // Title
     let title = makeDiv('title')
     title.textContent = pub_data.title;
 
+    // Authors
     let authors = makeDiv('authors')
     authors.textContent = pub_data.author.join(", ")
                             //  .replace(MY_NAME, '<span class="me">' + MY_NAME + '</span>');
     
+    // Venue
     let venue = makeDiv('venue')
     venue.textContent = pub_data.venue + ', ' + pub_data.year
 
 
     // add thumbnail
-    pub.appendChild(thumbnail)
+    pub.appendChild(thumbnail_link)
 
     // add pubinfo
     let pubInfo = makeDiv('pubinfo');
@@ -60,7 +74,6 @@ function addPub(pub_data) {
     pubInfo.appendChild(title)
     pubInfo.appendChild(authors)
     pubInfo.appendChild(venue)
-
 }
 
 main()
