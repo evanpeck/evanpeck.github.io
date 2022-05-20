1751818d0f3d5e2507328c471b316f0894834954
Evan <evanpeck@gmail.com>
2022-05-19 22:10:15 -0400

    Prototyping publications

--- _pages/publications.md
+++ _pages/publications.md
@@ -1,16 +1,19 @@
 ---
-layout: archive
+# layout: archive
 title: "Publications"
 permalink: /publications/
 author_profile: true
 ---
 
-{% if author.googlescholar %}
-  You can also find my articles on <u><a href="{{author.googlescholar}}">my Google Scholar profile</a>.</u>
-{% endif %}
+<link rel="stylesheet" href="{{ base_path }}/assets/css/pubstyle.css">
+
+**!!!** Note: This page is **heavily** under construction, so stick with me while I work on it. Until then, visit my current homepage at [http://www.eg.bucknell.edu/~emp017/](http://www.eg.bucknell.edu/~emp017/)
+{: .notice}
 
 {% include base_path %}
+<script src="{{ base_path }}/assets/js/listpubs.js"></script>
+<div id="publications"></div>
 
-{% for post in site.publications reversed %}
+<!-- {% for post in site.publications reversed %}
   {% include archive-single.html %}
-{% endfor %}
+{% endfor %} -->
---
+++ assets/js/listpubs.js
@@ -0,0 +1,64 @@
+
+const MY_NAME = "Evan M. Peck";
+const DIV_ID = "publications"
+const THUMBNAIL_PATH = "../../images/thumbnails/"
+const PUBS_JSON = "../../files/pubs.json"
+
+// Loads pubs JSON file
+async function getJSON() {
+    const response = await fetch(PUBS_JSON);
+    const json = await response.json();
+    return json
+}
+
+async function main() {
+    let pub_data = await getJSON();
+    let pubs = pub_data.publications;
+
+    pubs.forEach(pub => addPub(pub));
+}
+
+/**
+ * Helper function that returns classed div
+ * @param {*} className 
+ * @returns 
+ */
+function makeDiv(className) {
+    let div = document.createElement('div')
+    div.classList.add(className)
+    return div
+}
+
+function addPub(pub_data) {
+    let pubs_section = document.getElementById(DIV_ID);
+
+    let pub = makeDiv('pub')
+    pubs_section.appendChild(pub);
+
+    let thumbnail = document.createElement('img')
+    thumbnail.classList.add('thumbnail')
+    thumbnail.src = THUMBNAIL_PATH + pub_data.thumbnail
+
+    let title = makeDiv('title')
+    title.textContent = pub_data.title;
+
+    let authors = makeDiv('authors')
+    authors.textContent = pub_data.author.join(", ")
+                            //  .replace(MY_NAME, '<span class="me">' + MY_NAME + '</span>');
+    
+    let venue = makeDiv('venue')
+    venue.textContent = pub_data.venue + ', ' + pub_data.year
+
+    pub.appendChild(thumbnail)
+    pub.appendChild(title)
+    pub.appendChild(authors)
+    pub.appendChild(venue)
+
+    // author string
+    // let authors = 
+    // console.log(authors)
+
+    //
+}
+
+main()
